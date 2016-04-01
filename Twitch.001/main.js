fee0e0c590f42baf115190ca5437f5de99ec02fe
dick-ey <mdickey46@gmail.com>
2016-03-31 20:17:18 -0600

    js push

--- Twitch.001/main.js
+++ Twitch.001/main.js
@@ -1,1 +1,71 @@
-console.log('Javascript is working!');
+function startTime(){
+	var twelveHour = false;
+	var scale = "scale(1.0)";
+
+	// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+	var today = new Date();
+	var hour = today.getHours();
+	var minute = today.getMinute();
+	var meridiem = "";
+	var dayNames = new Array(
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+		);
+	var weekday = dayNames[today.getDay()];
+	var date = today.getDate();
+	var monthArray = new Array(
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+		);
+	var month = monthArray[today.getMonth()];
+
+
+	// FORMATTING
+
+	if (minute < 10) {
+		minute = "0" + minute;
+	}
+
+	if (twelveHour && hour >= 12){
+		meridiem = "pm";
+	}
+	if (twelveHour && hour < 12){
+		meridiem = "am";
+	}
+
+	// 12 hour time formatting
+	if (twelveHour && hour === 0){
+		hour = 12;
+	}
+
+	if (twelveHour){
+		hour = hour % 12;
+	}
+
+	if (!twelveHour && hour < 10){
+		hour = "0" + hour;
+	}
+
+	//Assignment statements
+	
+
+
+
+
+}
